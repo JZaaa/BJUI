fa1f57d2c4f0a54e9937ff88fe9c5cb93bf0b509
jzaaa <2509642078@qq.com>
2019-01-18 11:33:28 +0800

    - 修复bjui-lookup组件未实例化前添加newurl无效问题

--- BJUI/js/bjui-lookup.js
+++ BJUI/js/bjui-lookup.js
@@ -17,21 +17,21 @@
 
 +function ($) {
     'use strict';
-    
+
     // LOOKUP GLOBAL ELEMENTS
     // ======================
-    
+
     var group, suffix, $currentLookup
-    
+
     // LOOKUP CLASS DEFINITION
     // ======================
-    
+
     var Lookup = function(element, options) {
         this.$element = $(element)
         this.options  = options
         this.$lookBtn = null
     }
-    
+
     Lookup.DEFAULTS = {
         url       : null,
         id        : null,
@@ -42,14 +42,14 @@
         maxable   : true,
         resizable : true
     }
-    
+
     Lookup.EVENTS = {
         afterChange : 'afterchange.bjui.lookup'
     }
-    
+
     Lookup.prototype.init = function() {
         var that = this, options = this.options, tools = this.tools
-        
+
         if (!options.url) {
             BJUI.debug('Lookup Plugin: Error trying open a lookup dialog, url is undefined!')
             return false
@@ -62,28 +62,28 @@
             }
             options.url = encodeURI(options.url)
         }
-        
+
         group          = this.options.group  || null
         suffix         = this.options.suffix || null
         $currentLookup = this.$element
-        
+
         if (suffix) suffix = suffix.trim()
-        
+
         this.open(that.$element)
     }
-    
+
     Lookup.prototype.addBtn = function() {
         var that = this, $element = that.$element
-        
+
         if (!this.$lookBtn && !$element.parent().hasClass('wrap_bjui_btn_box')) {
             this.$lookBtn = $(FRAG.lookupBtn)
             this.$element.css({'paddingRight':'15px'}).wrap('<span class="wrap_bjui_btn_box"></span>')
-            
+
             var $box   = this.$element.parent()
             var height = this.$element.addClass('form-control').innerHeight()
-            
+
             $box.css({'position':'relative', 'display':'inline-block'})
-            
+
             $.each(that.options, function(key, val) {
                 if (key != 'toggle') that.$lookBtn.data(key, val)
             })
@@ -91,60 +91,60 @@
             this.$lookBtn.on('selectstart', function() { return false })
         }
     }
-    
+
     Lookup.prototype.open = function($obj) {
         var that = this, options = this.options
-        
+
         $obj.dialog({id:options.id || 'lookup_dialog', url:options.url, title:options.title, width:options.width, height:options.height, mask:options.mask, maxable:options.maxable, resizable:options.resizable})
     }
-    
+
     Lookup.prototype.getField = function(key) {
         return (group ? (group +'.') : '') + (key) + (suffix ? suffix : '')
     }
-    
+
     Lookup.prototype.setSingle = function(args,type) {
         if (typeof args == 'string')
             args  = new Function('return '+ args)()
         this.setVal(args,type)
     }
-    
+
     Lookup.prototype.setMult = function(id,type) {
         var args  = {}
         var $unitBox = this.$element.closest('.unitBox')
-        
+
         $unitBox.find('[name="'+ id +'"]').filter(':checked').each(function() {
             var _args = new Function('return '+ $(this).val())()
-            
+
             for (var key in _args) {
                 var value = args[key] ? args[key] +',' : ''
-                
+
                 args[key] = value + _args[key]
             }
         })
-        
+
         if ($.isEmptyObject(args)) {
             this.$element.alertmsg('error', this.$element.data('warn') || FRAG.alertSelectMsg)
             return
         }
-        
+
         this.setVal(args,type)
     }
-    
+
     Lookup.prototype.setVal = function(args, type) {
         var that = this
         var $box = $currentLookup.closest('.unitBox')
         var newValue  /* @description 增加 @author 小策一喋 */
-        
+
         // for datagrid
         if ($currentLookup.data('customEvent')) {
             $currentLookup.trigger('customEvent.bjui.lookup', [args])
         } else {
             $box.find(':input').each(function() {
                 var $input = $(this), inputName = $input.attr('name')
-                
+
                 for (var key in args) {
                     var name = that.getField(key)
-                    
+
                     if (name == inputName) {
 
                         /* @description 增加 追加参数 @author 小策一喋 */
@@ -156,29 +156,32 @@
                         $input
                             .val(newValue) /* @description 修改 args[key] 为 newValue @author 小策一喋 */
                             .trigger(Lookup.EVENTS.afterChange, {value:args[key]})
-                            
+
                         break
                     }
                 }
             })
         }
-        
+
         this.$element.dialog('closeCurrent')
     }
-       
+
     // LOOKUP PLUGIN DEFINITION
     // =======================
-    
+
     function Plugin(option) {
         var args     = arguments
         var property = option
-        
+
         return this.each(function () {
             var $this   = $(this)
             var options = $.extend({}, Lookup.DEFAULTS, $this.data(), typeof option == 'object' && option)
             var data    = $this.data('bjui.lookup')
-            
+
             if (!data) {
+                /*修复未实例化前添加newurl无效*/
+                if($this.data('newurl'))
+                    options.url = $this.data('newurl')
                 $this.data('bjui.lookup', (data = new Lookup(this, options)))
             } else if ($this.data('newurl')) {
                 data.options.url = $this.data('newurl')
@@ -198,49 +201,49 @@
 
     $.fn.lookup             = Plugin
     $.fn.lookup.Constructor = Lookup
-    
+
     // LOOKUP NO CONFLICT
     // =================
-    
+
     $.fn.lookup.noConflict = function () {
         $.fn.lookup = old
         return this
     }
-    
+
     // LOOKUP DATA-API
     // ==============
 
     $(document).on(BJUI.eventType.initUI, function(e) {
         var $this = $(e.target).find('[data-toggle="lookup"]')
-        
+
         if (!$this.length) return
-        
+
         Plugin.call($this, 'addBtn')
     })
-    
+
     $(document).on('click.bjui.lookup.data-api', '[data-toggle="lookupbtn"]', function(e) {
         var $this = $(this)
-        
+
         if ($this.attr('href') && !$this.data('url')) $this.attr('data-url', $this.attr('href'))
         if (!$this.data('title')) $this.attr('data-title', $this.text())
-        
+
         Plugin.call($this)
-        
+
         e.preventDefault()
     })
-    
+
     $(document).on('click.bjui.lookupback.data-api', '[data-toggle="lookupback"]', function(e) {
         var $this = $(this)
         var args  = $this.data('args')
         var mult  = $this.data('lookupid')
         var type = $('input[name="lookupType"]:checked').val() /* @description 新增 获取是否追加框值 @author 小策一喋 */
-        
+
         if (args)
             Plugin.call($this, 'setSingle', args, type) /* @description 修改 增加type参数 @author 小策一喋 */
         else if (mult)
             Plugin.call($this, 'setMult', mult, type) /* @description 修改 增加type参数 @author 小策一喋 */
-            
+
         e.preventDefault()
     })
-    
+
 }(jQuery);
