fb0516e1c73dabf26ee9761731f913ebf5ae165c
jzaaa <2509642078@qq.com>
2019-02-18 15:25:26 +0800

    - 新增BJUI插件自动压缩脚本

--- bin/bjui-js-optimize.bat
+++ bin/bjui-js-optimize.bat
@@ -7,7 +7,7 @@ pause
 cd %~dp0
 cd ..\BJUI\js
 
-uglifyjs jquery.cookie.js bjui-core.js bjui-regional.zh-CN.js bjui-frag.js bjui-extends.js bjui-basedrag.js bjui-slidebar.js bjui-contextmenu.js bjui-navtab.js bjui-dialog.js bjui-taskbar.js bjui-ajax.js bjui-alertmsg.js bjui-pagination.js bjui-util.date.js bjui-datepicker.js bjui-ajaxtab.js bjui-datagrid.js bjui-tablefixed.js bjui-tabledit.js bjui-spinner.js bjui-lookup.js bjui-tags.js bjui-upload.js bjui-theme.js bjui-initui.js bjui-plugins.js -c -m -o bjui-all.js
+uglifyjs jquery.cookie.js bjui-core.js bjui-regional.zh-CN.js bjui-frag.js bjui-extends.js bjui-modulefixed.js bjui-basedrag.js bjui-slidebar.js bjui-contextmenu.js bjui-navtab.js bjui-dialog.js bjui-taskbar.js bjui-ajax.js bjui-alertmsg.js bjui-pagination.js bjui-util.date.js bjui-datepicker.js bjui-ajaxtab.js bjui-datagrid.js bjui-tablefixed.js bjui-tabledit.js bjui-spinner.js bjui-lookup.js bjui-tags.js bjui-upload.js bjui-theme.js bjui-initui.js bjui-plugins.js -c -m -o bjui-all.js
 
 echo 压缩完成！
 pause & exit
