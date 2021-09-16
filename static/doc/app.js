8efc7fef56a4a44a7ce397aa126b32abe8d50791
jzaaa <2509642078@qq.com>
2021-09-16 14:25:11 +0800

    新增基本页面布局样式，优化文档样式

--- static/doc/app.js
+++ static/doc/app.js
@@ -4,7 +4,9 @@
     var $box = $(e.target)
     if (!$box.is(document)) {
       $box.find('code').each(function() {
-        const template = $(this).find('template')
+        const $that = $(this)
+        $that.closest('pre').wrap('<div class="bj-doc-code-grid"><div class="bj-doc--code-content"></div></div>')
+        const template = $that.find('template')
         if (template.length) {
           $(this).text(template.html())
           Prism.highlightElement(this)
