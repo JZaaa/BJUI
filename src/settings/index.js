608a2ffefcdae90b849afeb6e5f83e1848a7df4a
jzaaa <2509642078@qq.com>
2021-09-18 16:45:20 +0800

    update

--- src/settings/index.js
+++ src/settings/index.js
@@ -1,11 +1,5 @@
-import { ContentTypeEnum } from '@/js/util/enums/httpEnum'
 
 // 默认配置
 export default {
-  // 主页默认主页url
-  homeUrl: '/views/sys.html',
-  // 请求默认配置
-  request: {
-    contentType: ContentTypeEnum.JSON // 请求类型。 form-data格式ContentTypeEnum.FORM_URLENCODED
-  }
+
 }
