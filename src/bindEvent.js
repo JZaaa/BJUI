1fb624951ba42cb27db353ce2cd563df71248744
jzaaa <2509642078@qq.com>
2021-09-24 14:53:40 +0800

    添加窗口监听

--- src/bindEvent.js
+++ src/bindEvent.js
@@ -1,21 +1,47 @@
-import { menuContentSelector } from '@/utils/static'
+import { menuContentSelector, appSelector } from '@/utils/static'
 
-/**
- * 监听router变化
- * @private
- */
-window.addEventListener('hashchange', () => {
-  // 仅允许切换菜单页面
-  if ($.CurrentNavtab) {
-    const options = $.CurrentNavtab.data('options') || {}
-    const hash = options.hash
-    const currentHash = location.hash
-    if (hash && location.hash && hash !== currentHash) {
-      const $a = $(menuContentSelector).find(`a[data-hash="${currentHash}"]`)
-      if ($a.length) {
-        $(menuContentSelector).sidebar('changeActiveMenu', null, $a)
+(function() {
+  /**
+   * 监听router变化
+   * @private
+   */
+  window.addEventListener('hashchange', () => {
+    // 仅允许切换菜单页面
+    if ($.CurrentNavtab) {
+      const options = $.CurrentNavtab.data('options') || {}
+      const hash = options.hash
+      const currentHash = location.hash
+      if (hash && location.hash && hash !== currentHash) {
+        const $a = $(menuContentSelector).find(`a[data-hash="${currentHash}"]`)
+        if ($a.length) {
+          $(menuContentSelector).sidebar('changeActiveMenu', null, $a)
+        }
+      }
+    }
+    // $(menuContentSelector).sidebar('changeActiveMenu', location.hash)
+  })
+
+  let menuCollapsed = false
+
+  const resizeEvent = () => {
+    const width = $(document).width()
+    if (width < 900) {
+      if (!menuCollapsed && !$(appSelector).hasClass('menu-collapsed')) {
+        menuCollapsed = true
+        console.log(1)
+        $(menuContentSelector).sidebar('toggleCollapse')
+      }
+    } else {
+      if (menuCollapsed && $(appSelector).hasClass('menu-collapsed')) {
+        menuCollapsed = false
+        console.log(2)
+        $(menuContentSelector).sidebar('toggleCollapse')
       }
     }
   }
-  // $(menuContentSelector).sidebar('changeActiveMenu', location.hash)
-})
+  /**
+   * 监听窗口变化
+   */
+  window.addEventListener('resize', XEUtils.debounce(resizeEvent, 100))
+}())
+
