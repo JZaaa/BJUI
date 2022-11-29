2b459a8823eeb1984992a75129e44fe78fea9f8b
jzaaa <2509642078@qq.com>
2022-11-29 08:56:25 +0800

    新增内置$.ajaxRequest方法

--- BJUI/other/utils.js
+++ BJUI/other/utils.js
@@ -51,4 +51,97 @@
       }))
     }
   }
+
+  if (!$.ajaxRequest) {
+    /**
+     * ajax请求封装
+     * @param op 选项
+     * method {string} 请求类型
+     * url {string} 请求地址
+     * success {callable} 成功回调
+     * error {callable} 失败回调
+     * loading {boolean|$box} 是否存在加载框 默认false，需要指定 $.CurrentDialog 获取 $.CurrentNavtab
+     * loadingFunc {callable}
+     */
+    $.ajaxRequest = function (op) {
+      var contentType = op.contentType || 'application/json'
+      var data = op.data || {}
+      var method = op.method || 'GET'
+      $.ajax({
+        method: method,
+        url: op.url,
+        data: (contentType === 'application/json' && method.toUpperCase() === 'POST') ? JSON.stringify(data) : $.extend({}, data),
+        cache: false,
+        contentType: contentType,
+        dataType: op.dataType || 'json',
+        timeout: BJUI.ajaxTimeout,
+        success: function (json) {
+          if (!json[BJUI.keys.statusCode]) {
+            op.error && op.error(json)
+          } else {
+            if (json[BJUI.keys.statusCode] === BJUI.statusCode.error) {
+              if (json[BJUI.keys.message]) $('body').alertmsg('error', json[BJUI.keys.message])
+              op.error && op.error(json)
+            } else if (json[BJUI.keys.statusCode] === BJUI.statusCode.timeout) {
+              $('body').alertmsg('info', (json[BJUI.keys.message] || BJUI.regional.sessiontimeout))
+              BJUI.loadLogin()
+            } else if (json[BJUI.keys.statusCode] === BJUI.statusCode.ok) {
+              op.success && op.success(json)
+            } else {
+              op.error && op.error(json)
+            }
+          }
+        },
+        beforeSend: function () {
+          if (op.loadingFunc) {
+            op.loadingFunc(true)
+          } else if (op.loading) {
+            op.loading.loading()
+          }
+        },
+        complete: function () {
+          if (op.loadingFunc) {
+            op.loadingFunc(false)
+          } else if (op.loading) {
+            op.loading.loading(false)
+          }
+          if (op.complete) {
+            op.complete()
+          }
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+          op.error && op.error({
+            statusCode: xhr.status,
+            message: '请求失败'
+          })
+        },
+        statusCode: {
+          503: function (xhr, ajaxOptions, thrownError) {
+            $('body').alertmsg('error', FRAG.statusCode_503.replace('#statusCode_503#', BJUI.regional.statusCode_503) || thrownError)
+          },
+          // 添加 httpCode 403 超时弹框
+          403: function (xhr, ajaxOptions, thrownError) {
+            $('body').alertmsg('error', '登录超时' || thrownError)
+            BJUI.loadLogin()
+          },
+          // 添加 httpCode 401 无权限
+          401: function (xhr, ajaxOptions, thrownError) {
+            if (!BJUI.IS_DEBUG) {
+              $('body').alertmsg('error', '无权限访问' || thrownError)
+            }
+          },
+          404: function (xhr, ajaxOptions, thrownError) {
+            if (!BJUI.IS_DEBUG) {
+              $('body').alertmsg('error', ' httpCode: 404 .请求未找到！' || thrownError)
+            }
+          },
+          500: function (xhr, ajaxOptions, thrownError) {
+            if (!BJUI.IS_DEBUG) {
+              $('body').alertmsg('error', ' httpCode: 500 .请求失败！' || thrownError)
+            }
+          }
+        }
+      })
+    }
+  }
 }(jQuery))
