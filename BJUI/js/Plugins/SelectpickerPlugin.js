e3727325618f799c0895c20ba03b82fc97fcb6f1
jzaaa <2509642078@qq.com>
2024-08-26 14:36:33 +0800

    新增select自动定位

--- BJUI/js/Plugins/SelectpickerPlugin.js
+++ BJUI/js/Plugins/SelectpickerPlugin.js
@@ -22,8 +22,14 @@
         this._$el.attr('data-toggle', TOGGLE_ATTR)
       }
 
+
       const domData = this._$el.data()
       const options = Object.assign({}, BJUI.plugins.Selectpicker.DefaultConfigs, domData || {}, config || {})
+
+        // 分页
+      // if (this._$el.attr('data-toggle-change') === 'changepagesize') {
+      //   options.dropdownParent = null
+      // }
 
       this._plugin = new TomSelect(element, options)
 
@@ -36,6 +42,10 @@
       this._$el.data(NAME, this)
 
       return this._plugin
+    }
+
+    setVal(val) {
+        this._plugin.setValue(val)
     }
 
     dispose() {
@@ -64,18 +74,69 @@
 
   }
 
+  /**
+   * 自动展开定位
+   */
+  TomSelect.define('auto_position', function () {
+    const tomSelect = this
+    tomSelect.on('dropdown_open', () => {
+      const $control = $(tomSelect.control)
+      const offset = tomSelect.settings.dropdownParent === 'body' ? $control.offset() : $control.position();
+      offset.top += $control.outerHeight(true);
+
+      const $dropdown = $(tomSelect.dropdown)
+      const dropdownHeight = $dropdown.prop('scrollHeight') + 5; // 5 - padding value;
+      const controlPosTop = $control.get(0).getBoundingClientRect().top;
+      const wrapperHeight = $(tomSelect.wrapper).height();
+      const controlPosBottom = $control.get(0).getBoundingClientRect().bottom
+      const position =
+          controlPosTop + dropdownHeight + wrapperHeight > window.innerHeight &&
+          controlPosBottom - dropdownHeight - wrapperHeight >= 0 ?
+              'top' :
+              'bottom';
+      let w = tomSelect.wrapper.style.width !== 'fit-content' ? tomSelect.settings.dropdownParent === 'body' ? 'max-content' : '100%' : 'max-content';
+      const styles = {
+        width: w,
+        minWidth : $control.outerWidth(true),
+        left: offset.left
+      };
+
+      if (position === 'top') {
+        const styleToAdd = { bottom: offset.top, top: 'unset' };
+
+        if (tomSelect.settings.dropdownParent === 'body') {
+          styleToAdd.top = offset.top - $dropdown.outerHeight(true) - $control.outerHeight(true);
+          styleToAdd.bottom = 'unset';
+        }
+        Object.assign(styles, styleToAdd);
+        $dropdown.addClass('selectize-position-top');
+        $control.addClass('selectize-position-top');
+      } else {
+        Object.assign(styles, { top: offset.top, bottom: 'unset' });
+        $dropdown.removeClass('selectize-position-top');
+        $control.removeClass('selectize-position-top');
+      }
+
+      if (tomSelect.settings.dropdownParent !== 'body' && w === 'max-content' && $control.outerWidth(true) >= $dropdown.outerWidth(true)) {
+        w = '100%';
+      }
+
+      $dropdown.css(styles);
+    })
+  })
+
   BJUI.plugins.Selectpicker = {
     DefaultConfigs: {
-      plugins: ['no_backspace_delete'],
+      plugins: ['no_backspace_delete', 'auto_position'],
       create: false,
-      allowEmptyOption: true,
+      allowEmptyOption: false,
       copyClassesToDropdown: false,
       dropdownParent: 'body',
       render:{
         no_results: function( data, escape ){
           return '<div class="no-results">' + "\u65E0\u5339\u914D\u6570\u636E" + '</div>';
         },
-      }
+      },
     },
     Instance: SelectpickerPlugin,
   }
