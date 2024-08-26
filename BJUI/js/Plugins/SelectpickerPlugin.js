6f72f567817c0bc5a85a60791ad245114feb509a
jzaaa <2509642078@qq.com>
2024-08-26 19:00:28 +0800

    优化样式

--- BJUI/js/Plugins/SelectpickerPlugin.js
+++ BJUI/js/Plugins/SelectpickerPlugin.js
@@ -26,12 +26,35 @@
       const domData = this._$el.data()
       const options = Object.assign({}, BJUI.plugins.Selectpicker.DefaultConfigs, domData || {}, config || {})
 
-        // 分页
-      // if (this._$el.attr('data-toggle-change') === 'changepagesize') {
-      //   options.dropdownParent = null
-      // }
-
       this._plugin = new TomSelect(element, options)
+
+      // 新增样式
+      const setListStyle = () => {
+        if (this._plugin?.sifter?.items && this._plugin.sifter.items['']) {
+          const emptyDiv = this._plugin.sifter.items[''].$div
+          if (emptyDiv) {
+            emptyDiv.classList.add('empty-option')
+          }
+        }
+        this._plugin.off('dropdown_open', setListStyle)
+      }
+
+      const setTextStyle = (val) => {
+        if (val && val.length) {
+          this._plugin.control.classList.remove('empty-option')
+        } else {
+          this._plugin.control.classList.add('empty-option')
+        }
+      }
+
+      if (options.allowEmptyOption) {
+        this._plugin.on('dropdown_open', setListStyle)
+        this._plugin.on('change', (val) => {
+          setTextStyle(val)
+        })
+        setTextStyle(this._plugin.getValue())
+      }
+
 
       this._classWatcher = new BJUI.tools.Watcher.ClassWatcher(element, 'is-invalid', () => {
         this._plugin.wrapper.classList.add('is-invalid')
@@ -45,7 +68,7 @@
     }
 
     setVal(val) {
-        this._plugin.setValue(val)
+      this._plugin.setValue(val)
     }
 
     dispose() {
@@ -82,7 +105,7 @@
     tomSelect.on('dropdown_open', () => {
       const $control = $(tomSelect.control)
       const offset = tomSelect.settings.dropdownParent === 'body' ? $control.offset() : $control.position();
-      offset.top += $control.outerHeight(true);
+      offset.top += $control.outerHeight(true)
 
       const $dropdown = $(tomSelect.dropdown)
       const dropdownHeight = $dropdown.prop('scrollHeight') + 5; // 5 - padding value;
@@ -90,19 +113,19 @@
       const wrapperHeight = $(tomSelect.wrapper).height();
       const controlPosBottom = $control.get(0).getBoundingClientRect().bottom
       const position =
-          controlPosTop + dropdownHeight + wrapperHeight > window.innerHeight &&
-          controlPosBottom - dropdownHeight - wrapperHeight >= 0 ?
-              'top' :
-              'bottom';
+        controlPosTop + dropdownHeight + wrapperHeight > window.innerHeight &&
+        controlPosBottom - dropdownHeight - wrapperHeight >= 0 ?
+          'top' :
+          'bottom';
       let w = tomSelect.wrapper.style.width !== 'fit-content' ? tomSelect.settings.dropdownParent === 'body' ? 'max-content' : '100%' : 'max-content';
       const styles = {
         width: w,
-        minWidth : $control.outerWidth(true),
+        minWidth: $control.outerWidth(true),
         left: offset.left
       };
 
       if (position === 'top') {
-        const styleToAdd = { bottom: offset.top, top: 'unset' };
+        const styleToAdd = {bottom: offset.top, top: 'unset'};
 
         if (tomSelect.settings.dropdownParent === 'body') {
           styleToAdd.top = offset.top - $dropdown.outerHeight(true) - $control.outerHeight(true);
@@ -112,7 +135,7 @@
         $dropdown.addClass('selectize-position-top');
         $control.addClass('selectize-position-top');
       } else {
-        Object.assign(styles, { top: offset.top, bottom: 'unset' });
+        Object.assign(styles, {top: offset.top, bottom: 'unset'});
         $dropdown.removeClass('selectize-position-top');
         $control.removeClass('selectize-position-top');
       }
@@ -132,8 +155,8 @@
       allowEmptyOption: true,
       copyClassesToDropdown: false,
       dropdownParent: 'body',
-      render:{
-        no_results: function( data, escape ){
+      render: {
+        no_results: function (data, escape) {
           return '<div class="no-results">' + "\u65E0\u5339\u914D\u6570\u636E" + '</div>';
         },
       },
